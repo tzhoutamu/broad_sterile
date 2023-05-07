e3c92bf35540635f8fc3cd23e242508f9720c1fb
tzhou <tao_zhou0221@icloud.com>
2023-05-07 00:01:01 -0500

    includes most recent data (60*60*60 grid)

--- NEOS/NEOS.py
+++ NEOS/NEOS.py
@@ -196,7 +196,7 @@ class Neos:
             max_energy_fine_index = self.FindFineBinIndex(self.DataUpperBinEdges[i])
 
         W = self.get_width(set_name) # in meters, the detector total width is 2W
-        ndL = 3 # We integrate through L with ten points. It is probably enough.
+        ndL = 10 # We integrate through L with ten points. It is probably enough.
         dL = (2*W)/(ndL-1)
 
         for reactor in self.reactor_names:
--- PROSPECT/PROSPECT.py
+++ PROSPECT/PROSPECT.py
@@ -134,7 +134,7 @@ class Prospect:
         # we want to make events inside the data bin i.
 
         W = self.get_width(segment) # in meters, the detector total width is 2W
-        ndL = 3 # We only integrate through L with five points. It is probably enough.
+        ndL = 5 # We only integrate through L with five points. It is probably enough.
         dL = (2*W)/(ndL-1)
 
         for reactor in self.reactor_names:
